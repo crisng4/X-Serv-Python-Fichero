e07135e956c70fc5dda567362ab40f1bf9a16b5d
cristina nieto garcia <cnieto@zeta40.aulas.gsyc.es>
2016-02-02 13:00:43 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+fichero = open("/etc/passwd",'r')
+
+while 1:
+    lineas = fichero.readline()
+    argumentos = lineas.split(":")
+    usuario = argumentos[0]
+    shell = argumentos[-1]
+    print usuario, shell[:-1]
+    if not lineas:
+        break
+
+fichero.close()
+fichero = open("/etc/passwd",'r')
+print "Hay", len(fichero.readlines()), "usuarios"
+fichero.close()
